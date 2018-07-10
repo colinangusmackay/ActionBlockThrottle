1bf56cb470d12caabbbeecec76bc2c9641462426
Colin Mackay <github@colinmackay.scot>
2018-07-10 22:11:36 +0100

    Basic ActionBlock

---
+++ src/00.BasicSerialImplementation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _00.BasicSerialImplementation
+{
+    class Program
+    {
+        private static byte[] work = new byte[100];
+        static void Main(string[] args)
+        {
+            new Random().NextBytes(work);
+
+            for (int i = 0; i < work.Length; i++)
+            {
+                DoStuff(i);
+            }
+
+            Console.WriteLine("All done!");
+            Console.ReadLine();
+        }
+
+        static void DoStuff(int data)
+        {
+            int wait = work[data];
+            Console.WriteLine($"{data:D3} : Work will take {wait}ms");
+            Thread.Sleep(wait);
+        }
+    }
+}
---
+++ src/01.SimpleActionBlock/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace _01.SimpleActionBlock
+{
+    class Program
+    {
+        private static byte[] work = new byte[100];
+        static async Task Main(string[] args)
+        {
+            new Random().NextBytes(work);
+
+            // Define the throttle
+            ActionBlock<int> throttle = new ActionBlock<int>(i=>DoStuff(i));
+            
+            // Create the work set.
+            for (int i = 0; i < work.Length; i++)
+            {
+                throttle.Post(i);
+            }
+
+            // indicate that there is no more work 
+            throttle.Complete();
+
+            // Wait for the work to complete.
+            await throttle.Completion;
+
+            Console.WriteLine("All done!");
+            Console.ReadLine();
+        }
+
+        static void DoStuff(int data)
+        {
+            int wait = work[data];
+            Console.WriteLine($"{data:D3} : Work will take {wait}ms");
+            Thread.Sleep(wait);
+        }
+    }
+}
